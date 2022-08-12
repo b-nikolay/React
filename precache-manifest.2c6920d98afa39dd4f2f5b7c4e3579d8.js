2ad7af3a2300925e4dc38241c865d298c92f21e1
nikolay-b <barmin.nicolay@gmail.com>
2022-08-12 19:10:44 +0400

    Updates

---
+++ precache-manifest.2c6920d98afa39dd4f2f5b7c4e3579d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "55e2fc8963bbb0c0d667",
+    "url": "/React/static/css/main.e8f51712.chunk.css"
+  },
+  {
+    "revision": "55e2fc8963bbb0c0d667",
+    "url": "/React/static/js/main.eacb8117.chunk.js"
+  },
+  {
+    "revision": "5ff9ca7f43ac21ebfcde",
+    "url": "/React/static/js/runtime~main.cf3ed0c4.js"
+  },
+  {
+    "revision": "65021f880bad5fa043c9",
+    "url": "/React/static/js/2.0c3b6a30.chunk.js"
+  },
+  {
+    "revision": "41ce0ae518152fb152cb",
+    "url": "/React/static/css/3.b31de960.chunk.css"
+  },
+  {
+    "revision": "41ce0ae518152fb152cb",
+    "url": "/React/static/js/3.94c376c8.chunk.js"
+  },
+  {
+    "revision": "5c4bf7a537716e1ddb7a",
+    "url": "/React/static/css/4.6a51c551.chunk.css"
+  },
+  {
+    "revision": "5c4bf7a537716e1ddb7a",
+    "url": "/React/static/js/4.50603131.chunk.js"
+  },
+  {
+    "revision": "0697e2dea2d6b7d726ad69eec10ebd75",
+    "url": "/React/static/media/preloader.0697e2de.gif"
+  },
+  {
+    "revision": "402915aae8f94820d3095a63dd7e3738",
+    "url": "/React/index.html"
+  }
+];
